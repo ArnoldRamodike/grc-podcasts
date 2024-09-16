6617589a487f739ce6133c5b38c1a8fe4989e424
ArnoldRamodike <riccoarnold23@gmail.com>
2024-09-16 11:11:59 +0200

    Document compoents for adding, collaboarations

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  ClientSideSuspense,
+  LiveblocksProvider,
+  RoomProvider,
+} from "@liveblocks/react/suspense";
+import React from "react";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Loader from "./Loader";
+import ActiveCollaborators from "./ActiveCollaborators";
+
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-tittle">Share</p>
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
